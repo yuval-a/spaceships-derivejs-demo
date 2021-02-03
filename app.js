d5b3732f23937a50f5785e2b2859cc5074ed6e22
Yuval Aloni <yuval.invoke@gmail.com>
2021-02-04 00:57:03 +0200

    update for version 2 of Derive

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-require ("./Models")
+/* Uncomment to measure performance run time
+const { performance, PerformanceObserver } = require("perf_hooks")
+const perfObserver = new PerformanceObserver((items) => {
+  items.getEntries().forEach((entry) => {
+    console.log(entry)
+  })
+})
+
+perfObserver.observe({ entryTypes: ["measure"], buffer: true })
+*/
+
+
+require ('./models')
 .then(async Models=> {
- 
     // Save convinient references to our data models
     const PhotonTorpedos  = Models.PhotonTorpedos;
     const CrewMember = Models.CrewMember;
@@ -16,7 +27,8 @@ require ("./Models")
         return Promise.all([
             CrewMember.clear(),
             Cruiser.clear(),
-            Battleship.clear()
+            Battleship.clear(),
+            PhotonTorpedos.clear()
         ]);
     }
     async function init() {
@@ -24,61 +36,91 @@ require ("./Models")
         BoldlyGo = new Cruiser ("The Boldly Go");
         console.log ("Creating Feisty Battleship");
         Feisty = new Battleship ("The Feisty");
- 
+
         console.log ("Creating Ricard Crew Member");
         Ricard = new CrewMember("Ricard");
         Ricard.role = "captain";
         Ricard.rank = "captain";
- 
+        
         console.log ("Adding Ricard to Boldly Go")
         BoldlyGo.addCrew (Ricard);
         BoldlyGo.captain = Ricard;
- 
+
         console.log ("Creating Wort Crew Member");
         Wort = new CrewMember("Wort");
         Wort.role = "captain";
         Wort.rank = "commander";
- 
+
         console.log ("Adding Wort To Feisty");
         Feisty.addCrew(Wort);
-        Feisty.captain = Wort;
- 
+        Feisty.captain = {
+            $value: Wort,
+            $callback: ()=> {
+                console.log ("Wort was updated as the captain of the Feisty");
+            }
+        }
+
     }
  
     function restore() {
         return Promise.all([
             Cruiser.get("The Boldly Go"),
             Battleship.get("The Feisty")
-        ]);
+        ])
+        .catch(error=> {
+            console.log ("Error when restoring: " + error);
+            console.log ("Collections might not exist. You might not had run firstRun() first.");
+        });
     }
- 
+
     function battle() {
-        console.log ("Starting battle");
+        console.log ("\r\nStarting battle\r\n");
         BoldlyGo.raiseShields();
         Feisty.attack(BoldlyGo, 0);
     }
- 
-    console.log ("Clearing all...");
-    await clearAll();
-    init();
-    console.log ("Adding Photon Torpedos to Feisty");
-    // Wait until PhotonTorpedos are added to Feisty, then run a battle
-    Feisty.weapons.push( new PhotonTorpedos(function() {
-        console.log ("Photon Torpedos ready")
-        battle();
-    }) );
 
-    /* 
-    // You can also use Spaceship.map, but this is done here for the sake of example:
-    restore()
-    .then(ships=> {
-        BoldlyGo = ships[0];
-        Feisty   = ships[1];
-        BoldlyGo.lowerShields();
-        Feisty.attack(BoldlyGo, 0);
-    });
-    */
-    
+    async function firstRun() {
+        console.log ("Clearing all...");
+        await clearAll();
+        init();
+        Feisty.$_dbEvents.on("updated", (id, updatedFields)=> {
+            console.log ("Feisty updated ");
+            // console.dir (updatedFields, { depth: null });
+        });
+        BoldlyGo.$_dbEvents.on("updated", (id, updatedFields)=> {
+            console.log ("BoldlyGo updated ");
+            // console.dir (updatedFields, { depth: null });
+        });
+
+        (new PhotonTorpedos()).$_dbEvents.once("inserted", (id, torpedos)=> {
+            // load the Photon Torpedos into the weapons array of Feisty
+            console.log ("Loading Photon Torpedos into Feisty");
+            Feisty.weapons.push(
+            {
+                $value: torpedos,
+                // The callback will be run upon an update operation of the weapons property *of the Feisty Battleship*
+                $callback: battle
+            });
+        });
+    }
+
+    function notFirstRun() {
+        // You can also use Spaceship.map, but this is done here for the sake of example:
+        restore()
+        .then(ships=> {
+            BoldlyGo = ships[0];
+            Feisty   = ships[1];
+            BoldlyGo.lowerShields();
+            Feisty.attack(BoldlyGo, 0);
+        });
+    }
+
+    // performance.mark("run-start")
+    // Call firstRun for an example of a complete data initialization and updating
+    firstRun();
+
+    // Call notFirstRun (make sure to comment out firstRun() DO NOT RUN BOTH OF THEM!) to run actions assuming all data already exists.
+    // notFirstRun();
 })
 .catch (err=> {
     console.log ("Error initializing models: ",err);
--- models.js
+++ models.js
@@ -1,3 +1,5 @@
+// const { performance, PerformanceObserver } = require("perf_hooks")
+
 module.exports = new Promise( (resolve,reject)=> {
     var Models = {};
  
@@ -5,14 +7,17 @@ module.exports = new Promise( (resolve,reject)=> {
  
     derive.Model
     ({
+        // NOTE: ChangeStream ("collection events watcher") is NOT supported for non Replica Sets, so using a single local db instance, will not use it, and will fallback 
+        // to using "manual" event callbacks (this adds an O(N) run on each bulk inserts operation). To see Change Stream in action, change dbUrl to a Replica Set.
         dbUrl: "mongodb://localhost:27017/",
         dbName: "spaceshipyard",
-        debugMode: false,
-        defaultMethodsLog: true
+        debugMode: false, // <- Change this to true to see extensive DB related verbose information in real-time
+        defaultMethodsLog: true,
+
     })
     .then(
         async Model=> {
- 
+
             Models.Weapon = Model({
                 _TYPE:"",
                 _DAMAGE:-1,
@@ -29,29 +34,27 @@ module.exports = new Promise( (resolve,reject)=> {
                         return;
                     }
                     if (target.shields.up) {
+                        if (target.shields.percent == 0) {
+                            console.log ("Shields are already at 0 percent!");
+                            return;
+                        }
                         target.shields.percent -= this._DAMAGE;
                     }
                     else {
-                        target.integrityHull -= this._DAMAGE;
+                        if (target.hullIntegrity == 0) {
+                            console.log ("Hull integrity is already at 0!");
+                            return;
+                        }
+                        target.hullIntegrity -= this._DAMAGE;
                     }
                 },
-                _inserted: function() {
-                    if (this.$ready) this.$ready.call(this);
-                },
-                // For a weapon-ready callback
-                $ready: null
             }, "Weapon");
             
-            Models.PhotonTorpedos = class extends Models.Weapon
+            Models.PhotonTorpedos = Models.Weapon
             .derive({
                 _TYPE_: "Photon Torpedos",
                 _DAMAGE: 20
-            }) {
-                constructor(readyCallback) {
-                    super ();
-                    this.$ready = readyCallback;
-                }
-            };
+            });
             
             Models.CrewMember = Model({
                 _name: "",
@@ -66,7 +69,7 @@ module.exports = new Promise( (resolve,reject)=> {
                     up: false,
                     percent: 100
                 },
-                integrityHull: 100,
+                hullIntegrity: 100,
                 crew: [],
                 addCrew: function (crewMember) {
                     this.crew.push(crewMember);
@@ -82,7 +85,14 @@ module.exports = new Promise( (resolve,reject)=> {
                 captain: "",
                 
                 // Listen to changes on this properties
-                $Listen: [ "shields.percent", "integrityHull" ]
+                $Listen: [ "shields.percent", "hullIntegrity" ],
+                changed(prop, value) {
+                    // performance.mark("run-end");
+                    // performance.measure("run-time", "run-start", "run-end")
+                    console.log (this._name + ": " + prop + " CHANGED TO " + value);
+                    if (prop == "shields.percent" && value == 0) console.log (this._name + ": SHIELDS AT 0 PERCENT!");
+                    if (prop == "hullIntegrity"   && value == 0) console.log (this._name + ": HULL INTEGRITY IS AT 0! SHIP IS DESTROYED!");
+                }
             }, "Spaceship");
             
             Models.Cruiser = Models.Spaceship
@@ -97,6 +107,7 @@ module.exports = new Promise( (resolve,reject)=> {
                 attack: function(target, weaponIndex) {
                     Models.Weapon.get(this.weapons[weaponIndex])
                     .then(w=> {
+                        console.log (`${this._name} is attacking ${target._name}!`);
                         if (!w.armed) w.arm();
                         w.fire(target);
                     });
